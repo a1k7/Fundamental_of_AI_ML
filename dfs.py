8f760e59d7b90fc6794176410aca338242bd76c5
a1k7 <138591441+a1k7@users.noreply.github.com>
2026-03-30 22:27:19 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,16 +1,31 @@
-def dfs(tasks, path=None, best=None, best_cost=float('inf')):
-    from utils import total_cost
+from utils import calculate_cost
 
+
+def dfs(tasks, energy_level, max_time, path=None, best=None, best_cost=float('inf')):
+    
     if path is None:
         path = []
 
+    # Goal state
     if len(path) == len(tasks):
-        cost = total_cost(path)
-        return path, cost
+        cost = calculate_cost(path, energy_level, max_time)
+
+        if cost < best_cost:
+            return path, cost
+
+        return best, best_cost
 
     for t in tasks:
         if t not in path:
-            result, cost = dfs(tasks, path + [t], best, best_cost)
+            result, cost = dfs(
+                tasks,
+                energy_level,
+                max_time,
+                path + [t],
+                best,
+                best_cost
+            )
+
             if cost < best_cost:
                 best = result
                 best_cost = cost
